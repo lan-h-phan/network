0c6282dd56a196ce27fa8ea3b33d16474010066e
lan-h-phan <79010077+lan-h-phan@users.noreply.github.com>
2022-10-01 12:29:50 -0400

    Sounds for Characters
    I think?

--- js/config/characters.js
+++ js/config/characters.js
@@ -1,1 +1,17 @@
+// Instructor in the main menu & outro
+I = new Character({ align:"left", background:"#FFF", color:"#000", sound:"text_mid" });
 
+// The player in the main menu & outro
+p = new Character({ align:"right", background:"#333", color:"#bbb", sound:"text_low" });
+
+// instructor character, the one you can play
+i = new Character({ align:"left", background:"#79B8FE", sound:"text_mid" });
+
+// square participant
+s = new Character({ align:"right", background:"#FFF", sound:"text_high" });
+
+// circle participant
+c = new Character({ align:"right", background:"#333", color:"#bbb", sound:"text_low" });
+
+// rectangle participant
+r = new Character({ align:"right", background:"#FFF", sound:"text_high" });
